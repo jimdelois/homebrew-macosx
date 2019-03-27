37a6295566646975de67d74502909e43f10251bb
Jim DeLois <jimdelois@users.noreply.github.com>
2019-03-27 19:08:18 -0400

    Updating SHA256 for Corrected Package

--- marina.rb
+++ marina.rb
@@ -2,7 +2,7 @@ class Marina < Formula
   desc "A tool set to manage 'Stacks' of related Dockerized applications, with a specific focus on easing the process of local development"
   homepage "https://github.com/jimdelois/marina"
   url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-macos-x64-latest.tar.gz"
-  sha256 "87c95710f0105bae6daaeb70961bd441f354f298df23a08e99e15a631297652c"
+  sha256 "30cb884b892f504d866f680d91d39e3f060453c39ca1bef82026f4a35c47abdd"
   version "0.1.0"
 
   bottle :unneeded
