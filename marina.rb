b1bd0c4f1ca1c056bafc75ad05f7070ed79e4092
Jim DeLois <jimdelois@users.noreply.github.com>
2019-03-27 23:02:19 -0400

    Updating Versioning Scheme

--- marina.rb
+++ marina.rb
@@ -1,8 +1,8 @@
 class Marina < Formula
   desc "A tool set to manage 'Stacks' of related Dockerized applications, with a specific focus on easing the process of local development"
   homepage "https://github.com/jimdelois/marina"
-  url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-macos-x64-latest.tar.gz"
-  sha256 "30cb884b892f504d866f680d91d39e3f060453c39ca1bef82026f4a35c47abdd"
+  url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-0.1.0-macos-x64.tar.gz"
+  sha256 "c8297f7bd8679903db5c50ae7b39865aaf031e00d293c5b2c3eaece8b6c459e4"
   version "0.1.0"
 
   bottle :unneeded
