cad9b6cea8d2124c41af6291e22e74d485225315
Jim DeLois <jimdelois@users.noreply.github.com>
2019-03-29 17:10:59 -0400

    Version 0.1.1

--- marina.rb
+++ marina.rb
@@ -1,9 +1,9 @@
 class Marina < Formula
   desc "A tool set to manage 'Stacks' of related Dockerized applications, with a specific focus on easing the process of local development"
   homepage "https://github.com/jimdelois/marina"
-  url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-0.1.0-macos-x64.tar.gz"
-  sha256 "c8297f7bd8679903db5c50ae7b39865aaf031e00d293c5b2c3eaece8b6c459e4"
-  version "0.1.0"
+  url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-0.1.1-macos-x64.tar.gz"
+  sha256 "1afbeacefecf260bd42bf9260c4afb470d84adac1261c7422a55a42de02efc70"
+  version "0.1.1"
 
   bottle :unneeded
 
