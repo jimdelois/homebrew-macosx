15c059215a9bdcedf2fd2c5c12b3086dfc049489
Jim DeLois <jimdelois@users.noreply.github.com>
2019-03-29 17:52:36 -0400

    Updating Source Target to Release Asset

--- marina.rb
+++ marina.rb
@@ -1,7 +1,7 @@
 class Marina < Formula
   desc "A tool set to manage 'Stacks' of related Dockerized applications, with a specific focus on easing the process of local development"
   homepage "https://github.com/jimdelois/marina"
-  url "https://raw.githubusercontent.com/jimdelois/marina/master/dist/marina-0.1.1-macos-x64.tar.gz"
+  url "https://github.com/jimdelois/marina/releases/download/0.1.1/marina-0.1.1-macos-x64.tar.gz"
   sha256 "1afbeacefecf260bd42bf9260c4afb470d84adac1261c7422a55a42de02efc70"
   version "0.1.1"
 
